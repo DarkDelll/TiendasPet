635ff89e198ad60192971d99221357a61f0978b9
GabrielPaez <darkdelll@gmail.com>
2022-12-27 17:19:02 -0300

    primer commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavBar from './NavBar'
+
+const Header = () => {
+  return (
+    <header className='Header'>
+        <h1>Tienda Pet</h1>
+        <NavBar/>
+    </header>
+  )
+}
+
+export default Header
---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <nav className='Header__nav'>
+        <a href="">Productos</a>
+        <a href="">Contacto</a>
+    </nav>
+  )
+}
+
+export default NavBar
